3718e955a96b5a24c43857f25d737f48e1575169
andre <100816114+andrezinpy@users.noreply.github.com>
2022-03-20 23:25:30 -0300

    Add files via upload

---
+++ clear.py.py
@@ -0,0 +1,37 @@
+token = "ODcwNjY2MTQ0NDI2NjI3MDky.YQQFCQ.U-2GLF7Nzw3V4vCqmq8za4Oj4VY"
+prefix = "k!" 
+
+#--- BOT ---
+
+import discord
+from discord.ext import commands
+print("██╗  ██╗ ██████╗ ███████╗██╗  ██╗██")
+print("██║ ██╔╝██╔═══██╗██╔════╝██║ ██╔╝██║")
+print("█████╔╝ ██║   ██║███████╗█████╔╝ ██║")
+print("██╔═██╗ ██║   ██║╚════██║██╔═██╗ ██║")
+print("██║  ██╗╚██████╔╝███████║██║  ██╗██║")
+print("╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝");
+
+bot = commands.Bot(command_prefix=prefix, self_bot=True)
+bot.remove_command("help")
+
+@bot.event
+async def on_ready():
+    print("\n\n[Welcome] Ready! I await your command..") 
+    print(f"[Welcome] Name: {bot.user.name}")
+    print(f"[Welcome] ID: {bot.user.id}\n\n")
+
+@bot.command()
+async def clear(ctx, limit: int=None):
+    passed = 0
+    failed = 0
+    async for msg in ctx.message.channel.history(limit=limit):
+        if msg.author.id == bot.user.id:
+            try:
+                await msg.delete()
+                passed += 1
+            except:
+                failed += 1
+    print(f"[Complete] Removed {passed} messages with {failed} fails")
+
+bot.run(token, bot=False)
