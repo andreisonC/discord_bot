5369d4ed5ed4d7e55c9e84b8e7ca3cfd7a987324
andre <100816114+andrezinpy@users.noreply.github.com>
2022-03-22 23:08:50 -0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,9 +1,10 @@
 from email import message
 import discord
 import os
-
-from tqdm import tqdm 
+from tqdm import tqdm
 import time
+import datetime
+
 with tqdm(total=100) as pbar:
     for i in range(4):
         time.sleep(0.5)
@@ -20,25 +21,25 @@ class MyClient(discord.Client):
         print('Mensagem de {0.author}:{0.content}'.format(message))
         if message.content == '!regras':
             await message.channel.send(f'@{message.author.name} as regras do servidor são:{os.linesep}:point_right:1. Respeitar todos igualmente.{os.linesep}:point_right:3. Não gritar nas calls.{os.linesep}:point_right: 4. Não enviar pornográfia.{os.linesep}:point_right:5. Não enviar gore.{os.linesep}:point_right:6. Evitar off-topic.{os.linesep}:point_right:7. Usar bot de música somente nos canais de música.{os.linesep}')
-        elif message.content == '!nivel':
-            await message.author.send('Nivel ainda não foi programado :/')
+        elif message.content == '!instagram':
+            await message.author.send('https://www.instagram.com/andreison.py/')
+        elif message.content == "'info":
+            await message.channel.send(f'@{message.author.name} Mandei uma mensagem no seu privado, verifica lá!')
+            await message.author.send('Seila mano ')
 
 
-    async def on_menber_join(self,member):
+
+
+
+
+    async def on_menber_join(self, member):
         guild = member.guild
         if guild.system_channel is not None:
             mensagem = f'{member.mention} acabou de entrar no {guild.name}, seja bem vindo(a)!'
-            await guild.system_channel.send(message)
-
-    async def on_message(self, message):
-        print('Mensagem de {0.author}:{0.content}'.format(message))
-        if message.content == '!sexo':
-            await message.channel.send(f'@{message.author.name}, só mais tarde')
-        elif message.content == '<@!870666144426627092>':
-            await message.author.send(f'Oi, precisa de mim? Cheque meus comandos:{os.linesep}--------  Prefixo ! ---------- {os.linesep}!regras {os.linesep}!nivel')
+            await guild.system_channel.send(message)   
 
 
 intents = discord.Intents.default()
 intents.members = True
-client = MyClient(intents = intents)
-client.run('===============================')
+client = MyClient(intents=intents)
+client.run('===============================================M')
